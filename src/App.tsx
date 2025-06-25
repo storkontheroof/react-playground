e105deecda54dad98ef9e77fb58b1d95c8e0e1a6
Richard van den Winkel <richard.van.den.winkel@e-wise.nl>
2025-06-25 07:14:44 +0200

    renamed apps folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import LoadingSpinner from './components/LoadingSpinner';
-import Homepage from './apps/homepage/Homepage';
+import Homepage from './pages/homepage/Homepage';
 import Navbar from './components/Navbar';
 
-const ContextPage = React.lazy(() => import('./apps/context/App'));
+const ContextPage = React.lazy(() => import('./pages/context/App'));
 
 
 function App() {
